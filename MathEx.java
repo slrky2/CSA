fad01cc3a50c849af54f7d5191ae939d84e77702
slrky2 <144831052+slrky2@users.noreply.github.com>
2023-09-20 13:58:06 -0400

    Add files via upload

---
+++ MathEx.java
@@ -0,0 +1,18 @@
+public class MathEx{
+    public static void main(String[] args){
+        System.out.println(swap(164));
+    }
+    public static int swap(int x){
+        int answer2 = (x/10)%10;
+        int answer = x/100;
+        int answer3 = x%10;
+        int result = (answer3*100) +
+        return answer2;
+
+
+
+
+
+    }
+
+    }
